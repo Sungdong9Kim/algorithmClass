2b20ff14dba35a4059116d5f6efdb19823e37cdf
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-04-20 22:41:35 +0900

    feat : make new python file for reverse list

--- 210420_reverse_list.py
+++ 210420_reverse_list.py
@@ -1,11 +1,13 @@
 # 파라미터 some_list를 거꾸로 뒤집는 함수
 def flip(some_list):
 # 코드를 입력하세요.
-    if len(some_list) == 1:
+    if len(some_list) == 1 or len(some_list) == 0:
         return some_list
-    
+    return some_list[-1:] + flip(some_list[:-1])
+
 
 # 테스트
 some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 some_list = flip(some_list)
+#some_list = [9, 8, 7 ,6 ,5 ,4 ,3 ,2 ,1]
 print(some_list)
