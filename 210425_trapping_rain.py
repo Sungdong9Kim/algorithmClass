1b14f999fe9d2f369d8069ee839ad4a169b9fb9c
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-04-27 00:31:20 +0900

    feat : add function for trapping rain

--- 210425_trapping_rain.py
+++ 210425_trapping_rain.py
@@ -1,9 +1,13 @@
 def trapping_rain(buildings):
 # 코드를 작성하세요
-    left_max = -1
+    total_height = 0
+    for i in range(1, len(buildings) - 1):
+        max_left = max(buildings[:i])
+        max_right = max(buildings[i:])
 
-    for height in buildings:
+        upper_bound = min(max_left, max_right)
 
+        total_height += max(0, upper_bound - buildings[i])
 
 # 테스트
 print(trapping_rain([3, 0, 0, 2, 0, 4]))
