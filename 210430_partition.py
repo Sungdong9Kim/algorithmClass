c8415ce11afdbb1bdc39337bec61c18d9fcfa855
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-05-02 23:08:04 +0900

    feat : add function for partition

--- 210430_partition.py
+++ 210430_partition.py
@@ -8,21 +8,25 @@ def swap_elements(my_list, index1, index2):
 
 # 퀵 정렬에서 사용되는 partition 함수
 def partition(my_list, start, end):
-# 코드를 작성하세요.
-    i = b = start
-    if i == end:
-        swap_elements(my_list, b, i)
-    else:
-        if my_list[end] < my_list[i]:
-            i += 1
-        else:
-            swap_elements(my_list, b, i)
-            i += 1
+    # 리스트 값 확인과 기준점 이하 값들의 위치 확인을 위한 변수 정의
+    i = start
+    b = start
+    p = end
+
+    # 범위안의 모든 값들을 볼 때까지 반복문을 돌린다
+    while i < p:
+        # i 인덱스의 값이 기준점보다 작으면 i와 b 인덱스에 있는 값들을 교환하고 b를 1 증가 시킨다
+        if my_list[i] <= my_list[p]:
+            swap_elements(my_list, i, b)
             b += 1
-    return my_list
+        i += 1
 
+    # b와 기준점인 p 인덱스에 있는 값들을 바꿔준다
+    swap_elements(my_list, b, p)
+    p = b
 
-
+    # pivot의 최종 인덱스를 리턴해 준다
+    return p
 
 
 # 테스트 1
